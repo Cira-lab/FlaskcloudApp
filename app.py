62725dfc7bfebb41c45db1713572e5b5065e06a5
Cira-lab <ngangafaith83@gmail.com>
2025-03-06 18:17:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Praise Jesus,this is Faith Ng'ang'a 😊"
+    return "Praise Jesus,this is Faith Ng'ang'a"
 if __name__ == " __main__":
     app.run(debug=True)
